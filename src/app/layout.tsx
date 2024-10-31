c4baed4be4f32b0dcafa02fd9801adeea5367e78
JoaoBagvanji <jbagvanji54@gmail.com>
2024-10-31 12:55:52 -0300

    'Melhoria das rotas'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
-
 import "./globals.css";
-import Providers from "@/components/Providers";
+import Providers from "@/components/Providers"; // Providers já é um client component
 import AppBar from "@/components/AppBar";
+import SignInButton from "@/components/SignInButton";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import AuthenticatedLayout from "./AuthenticatedLayout";
 
 interface Props {
   children: React.ReactNode;
 }
 
-
-export default function RootLayout(props: Props){
-
+export default function RootLayout(props: Props) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          <AppBar />
-          {props.children}
+        <Providers> {/* O Providers precisa estar aqui para envolver todos os componentes */}
+          <AuthenticatedLayout>{props.children}</AuthenticatedLayout>
         </Providers>
       </body>
     </html>
